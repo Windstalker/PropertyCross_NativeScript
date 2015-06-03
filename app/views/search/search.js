51689b0d8be181cb12ab7d20e519b0c2512b8613
Alexander Subota <a.subota@mobidev.biz>
2015-06-03 16:03:54 +0300

    styling progress

--- app/views/search/search.js
+++ app/views/search/search.js
@@ -1,8 +1,17 @@
 var vmModule = require("./search_model");
+var viewModule = require("ui/core/view");
+var platform = require("platform");
 
 function pageLoaded(args) {
 	var page = args.object;
 	page.bindingContext = vmModule.model;
+
+	if (platform.device.os === 'iOS') {
+		var list = viewModule.getViewById(page, "locations");
+//        list.ios.allowsSelection = false;
+	}
+
+	console.log('loaded!');
 }
 
 function pageNavigatedTo(args) {
